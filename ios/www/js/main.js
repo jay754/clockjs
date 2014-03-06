c95fe4119ecb332116ee0f09ee2ce458e4113c86
Jay Engineer <jayengineer6@gmail.com>
2014-03-06 15:00:51 -0500

    code refactoring

--- ios/www/js/main.js
+++ ios/www/js/main.js
@@ -9,123 +9,123 @@ $(function(){
   /* Digital Time */
   
   function getTime(){
-  var d = new Date(); //initiate the time objects
-  var hours = d.getHours(); //get hours
-  var mins = d.getMinutes(); //get mins
-  var secs = d.getSeconds(); //get seconds
-  var timeofday = ''; //for the time of day pm or am
+    var d = new Date(); //initiate the time objects
+    var hours = d.getHours(); //get hours
+    var mins = d.getMinutes(); //get mins
+    var secs = d.getSeconds(); //get seconds
+    var timeofday = ''; //for the time of day pm or am
   
-  if(hours > 12){
-  timeofday += 'PM'; //if hours greater than 12 than it is pm
-  }
-  else{
-  timeofday += 'AM'; //else it is the morning time
-  }
+    if(hours > 12){
+        timeofday += 'PM'; //if hours greater than 12 than it is pm
+    }
+    else{
+        timeofday += 'AM'; //else it is the morning time
+    }
   
-  var actualtime = hours+' : '+mins+' : '+secs+' '+timeofday; //actual time of the day
+    var actualtime = hours+' : '+mins+' : '+secs+' '+timeofday; //actual time of the day
   
-  $('#clock').html(actualtime); //fill out the div
+    $('#clock').html(actualtime); //fill out the div
   }
   
   /* This is for pausing and resuming time */
   
   function buttons(i){
-  $('#pause').click(function() {
-                    clearInterval(i); //fill in the div with the actual time
-                    console.log('you clicked pause');
-                    });
-  $('#resume').click(function() {
-                     var i = setInterval(getTime, 1000); //clear interval
-                     $('#pause').click(function() {
-                                       //have to recurservly close the interval so now it can't open up again
-                                       
-                                       clearInterval(i); //fill in the div with the actual time
-                                       console.log('you clicked pause');
-                                       });
-                     console.log('you clicked resume'); //added interval workings
-                     });
+    $('#pause').click(function() {
+        clearInterval(i); //fill in the div with the actual time
+        console.log('you clicked pause');
+     });
+  
+    $('#resume').click(function() {
+        var i = setInterval(getTime, 1000); //clear interval
+        $('#pause').click(function(){
+
+            clearInterval(i); //fill in the div with the actual time
+            console.log('you clicked pause');
+        });
+        console.log('you clicked resume'); //added interval workings
+    });
   }
   
   /* This is for turning the clock into an alarm */
   
   function makeSound(){
-  var clickSound = new Audio('sounds/Alarm.mp3');
+    var clickSound = new Audio('sounds/Alarm.mp3');
   
-  $('#play').click(function(){
-                   clickSound.play();
-                   });
-  $('#stop').click(function(){
-                   clickSound.pause();
-                   });
+    $('#play').click(function(){
+        clickSound.play();
+      });
+    $('#stop').click(function(){
+        clickSound.pause();
+      });
   }
   
   function getAlarmTime(){
-  $('#settime').click(function(){
-                      var secs = parseInt($('#seconds').val());
-                      var clickSound = new Audio('sounds/alarm.mp3');
+    $('#settime').click(function(){
+     var secs = parseInt($('#seconds').val());
+     var clickSound = new Audio('sounds/alarm.mp3');
                       
-                      var d = String(new Date().getTime() / 1000);
-                      var currentTime = parseInt(d.split('.'));
-                      addedTime = secs + currentTime;
-                      localStorage.setItem('time', addedTime);
+     var d = String(new Date().getTime() / 1000);
+     var currentTime = parseInt(d.split('.'));
+     addedTime = secs + currentTime;
+     localStorage.setItem('time', addedTime);
                       
-                      $('.notification').html('Your time has been updated. The Alarm will go off in ' + secs);
-                      $('.notification').addClass('alert alert-success');
+     $('.notification').html('Your time has been updated. The Alarm will go off in ' + secs);
+     $('.notification').addClass('alert alert-success');
                       
-                      console.log(addedTime);
-                      });
+     console.log(addedTime);
+    });
   }
   
   function playAlarm(){
-  var clickSound = new Audio('sounds/Alarm.mp3');
+    var clickSound = new Audio('sounds/Alarm.mp3');
   
-  var d = String(new Date().getTime() / 1000);
-  var currentTime = parseInt(d.split('.'));
-  var alarmtime = localStorage.getItem('time');
+    var d = String(new Date().getTime() / 1000);
+    var currentTime = parseInt(d.split('.'));
+    var alarmtime = localStorage.getItem('time');
   
-  if (alarmtime != null){
-  if (currentTime > alarmtime){
-  window.localStorage.clear();
-  clickSound.play();
-  }
-  }
+    if (alarmtime != null){
+        if (currentTime > alarmtime){
+            window.localStorage.clear();
+            clickSound.play();
+        }
+    }
   
-  console.log(currentTime);
-  console.log(alarmtime);
+    console.log(currentTime);
+    console.log(alarmtime);
   }
   
   function timeLeft(){
-  var d = String(new Date().getTime() / 1000);
-  var currentTime = parseInt(d.split('.'));
-  var alarmtime = localStorage.getItem('time');
+    var d = String(new Date().getTime() / 1000);
+    var currentTime = parseInt(d.split('.'));
+    var alarmtime = localStorage.getItem('time');
   
-  var timeleft = alarmtime - currentTime;
+    var timeleft = alarmtime - currentTime;
   
-  if(timeleft > 0){
-  $('.timeleft').html('Time Left: '+ timeleft);
-  }
-  else if(timeleft === 0){
-  sleep(1000);
-  location.reload();
-  }
+    if(timeleft > 0){
+        $('.timeleft').html('Time Left: '+ timeleft);
+    }
+    else if(timeleft === 0){
+        sleep(1000);
+        location.reload();
+    }
   }
   
   /* Testing it first on the console to make sure it works */
   
   function getConsoleTime(){
-  var d = new Date();
-  var hours = d.getHours();
-  var mins = d.getMinutes();
-  var secs =d.getSeconds();
+    var d = new Date();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs =d.getSeconds();
   
-  console.log(hours+':'+mins+':'+secs);
+    console.log(hours+':'+mins+':'+secs);
   }
   
   //for stopping delaying the one second after the page refreshes
   
   function sleep(delay) {
-  var start = new Date().getTime();
-  while (new Date().getTime() < start + delay);
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
   }
   
   var i = setInterval(getTime, 1000);
